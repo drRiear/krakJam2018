2cbb91aa5bf1b666857f6f729202282667e434c9
drRiear <yurii.danylenko@gmail.com>
2018-01-26 19:32:25 +0100

    Camera. proc

--- Assets/Scripts/Enemy/EnemyCameraBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyCameraBehaviour.cs
@@ -5,20 +5,60 @@ using UnityEngine;
 public class EnemyCameraBehaviour : MonoBehaviour {
 
     #region Inspector Vars
-
+    [Header("Rotation")]
     [SerializeField] private float speedOfRotation;
     [SerializeField] private float angleOfRotation;
+    [Header("View")]
+    [SerializeField] private float distanceOfView;
+    [SerializeField] private float angleOfView;
     #endregion
 
+    #region Private Vars
+    [SerializeField] private State state = 0;
 
-    void Start () {
-		
+    private Quaternion startRotation;
+    private Quaternion destinationRotation = new Quaternion();
+    private float rototionLerpT;
+    #endregion
+
+    #region Unity Events
+    void Start ()
+    {
+        startRotation = transform.rotation;
+        destinationRotation.eulerAngles = new Vector3(0.0f, 0.0f, angleOfRotation);
+    }
+	void Update ()
+	{
+	    SetState();
 	}
-	
-	
-	void Update () {
-		
-	}
+    #endregion
+    
+    #region Private methods
+    private void SetState()
+    {
+        
+        switch (state)
+        {
+            case State.Idle:
+
+                break;
+            case State.Rotating:
+
+                SetRotation(rototionLerpT);
+                rototionLerpT += Time.deltaTime;
+
+                break;
+        }
+    }
+
+    private void SetRotation(float _t)
+    {
+        var currentZRotation = transform.rotation.z;
+        var newZRotation = Mathf.Lerp(currentZRotation, destinationRotation.z, _t);
+        //transform.Rotate();
+    }
+
+    #endregion
 
     private enum State { Idle, Rotating };
 }
