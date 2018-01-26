4e668293a81d229242fe3ffbbb547a95c69f4962
Szymon Przybyszewski <kapitankopytko@gmail.com>
2018-01-26 19:55:24 +0100

    Added player shooting sound

---
+++ Assets/Scripts/Player/ShootingScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
