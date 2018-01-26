d15e6d1ed08fc960733498eacad70424a2b6d45a
drRiear <yurii.danylenko@gmail.com>
2018-01-26 17:54:01 +0100

    CameraFollow. start

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform playerTransform;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
