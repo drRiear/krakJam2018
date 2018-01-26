e78e8e2ccd514c31f731d0427207653498d9e2b2
Axandar <axandar29@gmail.com>
2018-01-26 23:26:15 +0100

    Added BlockSwitcher

---
+++ Assets/Scripts/Hacking/BlockSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSwitcher : MonoBehaviour {
+
+    #region Variables
+    [SerializeField] private List<GameObject> list = new List<GameObject>();
+    [SerializeField] private float delay;
+    [SerializeField] private float interval;
+    #endregion
+
+    private int actualIndex = 0;
+
+    void Start () {
+        InvokeRepeating("ChangeCollision", delay, interval);
+    }
+	
+	
+	void Update () {
+        
+    }
+
+    void ChangeCollision() {
+        int previousIndex;
+        if(actualIndex == 0) {
+            previousIndex = list.Count-1;
+        } else
+            previousIndex = actualIndex - 1;
+
+        list[actualIndex].GetComponent<SpriteRenderer>().enabled = false;
+        list[actualIndex].GetComponent<Collider2D>().enabled = false;
+
+        list[previousIndex].GetComponent<SpriteRenderer>().enabled = true;
+        list[previousIndex].GetComponent<Collider2D>().enabled = true;
+
+        if(actualIndex == list.Count-1)
+            actualIndex = 0;
+        else
+            actualIndex += 1;
+    }
+}
