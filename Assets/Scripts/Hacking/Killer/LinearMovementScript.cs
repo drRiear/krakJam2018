70e1fe53380d5894679126154d06dfc468d6677f
Axandar <axandar29@gmail.com>
2018-01-26 22:30:37 +0100

    Added firewall

--- Assets/Scripts/Hacking/Killer/LinearMovementScript.cs
+++ Assets/Scripts/Hacking/Killer/LinearMovementScript.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class LinearMovementScript : MonoBehaviour {
 
     #region Variables
-    [SerializeField] private float speed,destination;
+    [SerializeField] private float speed;
+    [SerializeField] private int distance;
+    [SerializeField] private bool isOX;
     #endregion
 
-    Vector3 startPos,destPos;
-    int direction = 1;
+    private Vector3 startPos, destPos;
+    private int direction = 1;
 
     void Start()
     {
         startPos = transform.position;
-        destPos = new Vector3(startPos.x + destination, startPos.y);
-        
-        
+        if(isOX) {
+            destPos = new Vector3(startPos.x + distance, startPos.y);
+        } else {
+            destPos = new Vector3(startPos.x, startPos.y + distance);
+        }
     }
 
 
 	void Update () {
-        transform.Translate(transform.right * Time.deltaTime * speed*direction);
+        if(isOX) {
+            transform.Translate(transform.right * Time.deltaTime * speed*direction);
+        } else {
+            transform.Translate(transform.up * Time.deltaTime * speed*direction);
+        }
         
-        if (Vector3Int.RoundToInt(transform.position) == Vector3Int.RoundToInt(destPos))
-        {
+        if (Vector3Int.RoundToInt(transform.position) == Vector3Int.RoundToInt(destPos)){
             
             transform.localRotation = Quaternion.Euler(180, 0, 0);
             direction = -1;
-        }
-        else if (Vector3Int.RoundToInt(transform.position) == Vector3Int.RoundToInt(startPos))
-        {
+        }else if (Vector3Int.RoundToInt(transform.position) == Vector3Int.RoundToInt(startPos)){
             
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             direction = 1;
