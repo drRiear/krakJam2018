90bedd47b738547f31692c7911081af33dbee304
drRiear <yurii.danylenko@gmail.com>
2018-01-26 21:44:41 +0100

    Camera rotation

--- Assets/Scripts/Enemy/EnemyCameraBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyCameraBehaviour.cs
@@ -7,21 +7,20 @@ public class EnemyCameraBehaviour : MonoBehaviour {
     #region Inspector Vars
     [Header("Rotation")]
     [SerializeField] private float angleOfRotation;
-    [Header("View")]
-    [SerializeField] private float distanceOfView;
-    [SerializeField] private float angleOfView;
+    [SerializeField] private float rotationSpeed;
+    [Header("Idle")]
     [SerializeField] private float inIdleTime;
     #endregion
 
     #region Private Vars
-    [SerializeField] private State state = 0;
+    private State state = 0;
 
     private Quaternion startRotation;
     private Quaternion finalRotation;
 
     private Quaternion destinationRotation;
 
-    private float rotationLerpT;
+    private float rotationDirection = 1;
 
     private float inIdleTimer;
     #endregion
@@ -45,8 +44,6 @@ public class EnemyCameraBehaviour : MonoBehaviour {
         switch (state)
         {
             case State.Idle:
-                rotationLerpT = 0.0f;
-
                 inIdleTimer -= Time.deltaTime;
 
                 if (inIdleTimer <= 0.0f)
@@ -54,42 +51,36 @@ public class EnemyCameraBehaviour : MonoBehaviour {
 
                 break;
             case State.Rotating:
-
                 inIdleTimer = inIdleTime;
 
                 SetRotation();
-
-                //print(rotationLerpT);
-                
 
                 break;
         }
     }
 
-    public float rotationDirection = 1;
-    private float rotationSpeed = 20;
-
-    private void SetDestinationZRotation()
-    {
-        if (transform.rotation == startRotation)
-            rotationDirection = 1;
-        if (transform.rotation == finalRotation)
-            rotationDirection = -1;
-    }
-
     private void SetRotation()
     {
-        SetDestinationZRotation();
+        SetDestinationRotation();
 
-        var newRotation = Quaternion.RotateTowards(transform.rotation, finalRotation, Time.deltaTime * rotationDirection * rotationSpeed);
-
-        print(newRotation);
-
-        transform.rotation = newRotation;
-
-        rotationLerpT += Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, destinationRotation,
+                Time.deltaTime * rotationDirection * rotationSpeed);
     }
 
+    private void SetDestinationRotation()
+    {
+        if (transform.rotation == startRotation)
+        {
+            state = State.Idle;
+            destinationRotation = finalRotation;
+        }
+
+        if (transform.rotation == finalRotation)
+        {
+            state = State.Idle;
+            destinationRotation = startRotation;
+        }
+    }
     #endregion
 
     private enum State { Idle, Rotating };
