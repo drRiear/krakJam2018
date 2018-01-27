c469943ffa6fd134d77669869c58ce06644e4b5c
Cainie <karol.marszalik@gmail.com>
2018-01-27 13:15:55 +0100

    Adding Triggerable Enemy and Trigger Prefab. Includes neccesary scripts

---
+++ Assets/Scripts/Hacking/OnTriggerEnemyScript/MovementStarter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStarter : MonoBehaviour {
+
+    #region
+    [SerializeField] private TriggerEnemyMovement trigger;
+    #endregion
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            trigger.Trigger = true;
+        }
+
+    }
+
+}
