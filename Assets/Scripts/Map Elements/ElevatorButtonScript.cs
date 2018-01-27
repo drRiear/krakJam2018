210b442ad08c36b3efdb14567722cff91438f2fb
Szymon Przybyszewski <kapitankopytko@gmail.com>
2018-01-27 21:24:14 +0100

    Elevator Scripts (Go to next level)

---
+++ Assets/Scripts/Map Elements/ElevatorButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ElevatorButtonScript : MonoBehaviour
+{
+
+
+    public AudioSource openElevatorSound;
+
+    void Start()
+    {
+        openElevatorSound = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            openElevatorSound.Play();
+            Invoke("OpenElevator", 1.5f);
+            
+        }
+    }
+
+    void OpenElevator()
+    {
+        //SceneManager.LoadScene(NASTEPNA SCENA);
+    }
+
+}
---
+++ Assets/Scripts/Map Elements/ElevatorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour {
+
+
+    public static Collider2D collider;
+    public static SpriteRenderer renderer;
+
+    public static AudioSource elevatorSound;
+
+	void Start () {
+        collider = GetComponent<BoxCollider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        elevatorSound = GetComponent<AudioSource>();
+        	
+	}
+}
