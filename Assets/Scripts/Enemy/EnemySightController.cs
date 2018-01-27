7aa2d1c982175ba860dd8119fd786fa03ac37e54
Szymon Przybyszewski <kapitankopytko@gmail.com>
2018-01-27 01:40:22 +0100

    Enemy And Camera Sight Triggers

--- Assets/Scripts/Enemy/EnemySightController.cs
+++ Assets/Scripts/Enemy/EnemySightController.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySightController : MonoBehaviour {
-
-    Transform viewScale;
-	
-	// Update is called once per frame
-	void Update () {
-		if (GameController.isDetected == true)
-        {
-          
-        }
+public class EnemySightController : MonoBehaviour
+{
 
 
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameController.isDetected = true;
+    }
 
+    void Update()
+    {
+        if (GameController.isDetected == true)
+         Attack();
+        
+    }
 
+    void Attack()
+    {
 
-
+    }
 }
