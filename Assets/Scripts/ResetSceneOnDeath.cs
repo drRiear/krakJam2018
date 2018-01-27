c5d76d066bf5d7a34862d47d09341c36367f86ac
Cainie <karol.marszalik@gmail.com>
2018-01-27 17:08:01 +0100

    Staging ResetSceneOnDeath Script

---
+++ Assets/Scripts/ResetSceneOnDeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetSceneOnDeath : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        
+        spriteRenderer.enabled = false;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            spriteRenderer.enabled = true;
+
+            Invoke("Reset", 2);
+        }
+            
+    }
+
+    void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
