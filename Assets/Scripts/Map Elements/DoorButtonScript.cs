7bad247dc799843c82891f8b10bb43f094e4afb5
Szymon Przybyszewski <kapitankopytko@gmail.com>
2018-01-27 18:07:13 +0100

    Door Button + Door Prefabs + TITUTITU Sounds

---
+++ Assets/Scripts/Map Elements/DoorButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorButtonScript : MonoBehaviour
+{
+
+
+    public AudioSource openDoorSound;
+
+    void Start()
+    {
+        openDoorSound = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            openDoorSound.Play();
+            Invoke("OpenDoor", 1.5f);
+            
+        }
+    }
+
+    void OpenDoor()
+    {
+        DoorScript.collider.enabled = false;
+        DoorScript.renderer.enabled = false;
+        DoorScript.doorSound.Play();
+    }
+
+}
