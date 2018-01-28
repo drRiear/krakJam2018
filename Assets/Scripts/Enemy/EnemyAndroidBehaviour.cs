55ab460961115c8e6be72b3f00279429adc28a50
drRiear <yurii.danylenko@gmail.com>
2018-01-28 01:41:16 +0100

    stealth level

--- Assets/Scripts/Enemy/EnemyAndroidBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyAndroidBehaviour.cs
@@ -8,13 +8,18 @@ public class EnemyAndroidBehaviour : MonoBehaviour
     [SerializeField] private float inIdleTime;
 
     [SerializeField] private List<Transform> waypointsList;
-    [SerializeField] private List<Transform> waypointsQueueList;
+    private List<Transform> waypointsQueueList;
 
-    public Transform lastWaypoint;
+    private Transform lastWaypoint;
     private float timer;
     private State state;
     private Vector3 direction;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.grey;
+        Gizmos.DrawRay(transform.position, direction.normalized * distanceOfView);
+    }
     private void Start()
     {
         timer = inIdleTime;
@@ -27,26 +32,50 @@ public class EnemyAndroidBehaviour : MonoBehaviour
     }
     private void Update()
     {
+        LookForPlayer();
+
         switch (state)
         {
             case State.Patrol:
-            Patrol();
+                Patrol();
                 break;
             case State.Idle:
-            Idle();
+                Idle();
                 break;
             case State.Pursuit:
-
+                Pursuit();  
                 break;
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        int wallLayer = (1 << CharacterManager.Instance.wallsLayer.value) + 7;
+        if (collision.gameObject.layer == wallLayer)
+            state = State.Idle;
+        
+        var deathComponent = collision.gameObject.GetComponent<PlayerDeath>();
 
+        if (deathComponent == null) return;
+
+        deathComponent.Die();
+    }
 
     #region Private Methods
     private void SetExeptions()
     {
         if (waypointsList.Count < 1)
             Debug.LogWarning("Waypoints not setted.");
+    }
+
+    private void LookForPlayer()
+    {
+        var hits = Physics2D.RaycastAll(transform.position, direction.normalized, distanceOfView);
+
+        foreach(var hit in hits)
+        {
+            if (hit.collider.gameObject == CharacterManager.Instance.player)
+                state = State.Pursuit;
+        }
     }
 
     #region State Methods
@@ -73,6 +102,17 @@ public class EnemyAndroidBehaviour : MonoBehaviour
             state = State.Patrol;
     }
 
+    private void Pursuit()
+    {
+        Transform playerTransform = CharacterManager.Instance.player.transform;
+
+        direction = playerTransform.position - transform.position;
+
+        SetRotation(direction.normalized);
+
+        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position,
+            Time.deltaTime * speed);
+    }
     #endregion
 
     private void SetLastWaypoint()
@@ -84,10 +124,8 @@ public class EnemyAndroidBehaviour : MonoBehaviour
     private void MoveToWaypoint()
     {
         direction = waypointsQueueList[0].position - transform.position;
-
-        print(direction.normalized);
-
-        SetRotationDirection(direction.normalized);
+        
+        SetRotation(direction.normalized);
 
         transform.position = Vector3.MoveTowards(transform.position, waypointsQueueList[0].position, Time.deltaTime * speed);
 
@@ -111,7 +149,7 @@ public class EnemyAndroidBehaviour : MonoBehaviour
         waypointsQueueList.Reverse();
     }
 
-    private void SetRotationDirection(Vector2 dir)
+    private void SetRotation(Vector2 dir)
     {
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
